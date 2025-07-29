39ebd2cf725bba98d90b2be5b0666da5ff3c85fd
Louis Rey <rlhardrock@gmail.com>
2025-07-28 22:22:55 -0500

    fix3 link to dashboard

--- src/app/features/users/user-resumen.page.ts
+++ src/app/features/users/user-resumen.page.ts
@@ -15,8 +15,9 @@ import { UserService } from '../../shared/services/user.service';
            class="bg-purple-600 text-white px-4 py-2 rounded hover:bg-purple-700">
           Ir al Tablero de Mando
         </a>
+        <br>
       </div>
-      <div class="bg-gray-100 p-4 rounded shadow mb-4 space-y-1">
+      <div class="bg-gray-200 p-4 rounded shadow mb-4 space-y-1">
         <p><strong>Licencia:</strong> {{ data?.licenseSup }}</p>
         <p><strong>Nombre:</strong> {{ data?.name }}</p>
         <p><strong>Apellido:</strong> {{ data?.lastName }}</p>
@@ -29,7 +30,7 @@ import { UserService } from '../../shared/services/user.service';
         <p><strong>Fecha de nacimiento:</strong> {{ data?.dateBirth | date: 'longDate' }}</p>
       </div>
 
-      <a routerLink="/users" (click)="clearStorage()" class="text-blue-600 underline"> Volver al listado</a>
+      <a routerLink="/users" (click)="clearStorage()" class="bg-blue-600 text-white-600 px-4 py-2 rounded hover:bg-green-600"> Volver al listado</a>
 
     </div>
   `
